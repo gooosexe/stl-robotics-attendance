57bb970ce2baf8096a8f7c1505b42e2d94904d94
Oliver Huang <olivershaohuang2006@gmail.com>
2023-08-24 21:01:31 -0400

    reformatting

--- server/src/index.js
+++ server/src/index.js
@@ -9,59 +9,61 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function isValidUser(providedUsername, providedPassword){
-    // Open comma seperated values file
+function isValidUser(providedUsername, providedPassword) {
+  // Open comma seperated values file
 
-    let fs = require('fs');
-    let rawdata = fs.readFileSync('./src/users.csv');
-    let users = rawdata.toString().split("\n");
-    for (let user of users){
-        const [team, name, permission, username, password] = user.split(",");
-        if (providedUsername == username.trimEnd() && providedPassword == password.trimEnd()) {
-            return [true, permission, name, team]; 
-        }
+  let fs = require("fs");
+  let rawdata = fs.readFileSync("./src/users.csv");
+  let users = rawdata.toString().split("\n");
+  for (let user of users) {
+    const [team, name, permission, username, password] = user.split(",");
+    if (
+      providedUsername == username.trimEnd() &&
+      providedPassword == password.trimEnd()
+    ) {
+      return [true, permission, name, team];
     }
-    return [false, "none"];
+  }
+  return [false, "none"];
 }
 
-
-
-function authenticate(req, res, next){
-    const {authorization} = req.headers;
-    if (authorization){
-        const token = authorization.split(" ")[1];
-        const decoded = jwt.verify(token, "secretKey");
-        if (decoded){
-            next();
-        } else {
-            res.json({error: "invalid"});
-        }
+function authenticate(req, res, next) {
+  const { authorization } = req.headers;
+  if (authorization) {
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, "secretKey");
+    if (decoded) {
+      next();
     } else {
-        res.status(401).json({error: "Unauthorized"});
+      res.json({ error: "invalid" });
     }
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
 }
 
-app.get('/protected', authenticate, (req, res) => {
-    res.json({message: "You are authorized"});
+app.get("/protected", authenticate, (req, res) => {
+  res.json({ message: "You are authorized" });
 });
-
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!" });
 });
 
-app.post('/login', (req, res) => {
-    const {username, password} = req.body; 
-    const [isValid, permission, name, team] = isValidUser(username, password);
-    
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const [isValid, permission, name, team] = isValidUser(username, password);
 
-    if (isValid){   
-        const token = jwt.sign({username: username, permission: permission, name: name, team: team}, "secretKey"); 
-        res.json({token: token}); 
-    } else {
-        res.json({token: "invalid"});
-        // res.status(401).json({error: "Invalid Username or Password"});
-    }
+  if (isValid) {
+    const token = jwt.sign(
+      { username: username, permission: permission, name: name, team: team },
+      "secretKey"
+    );
+    res.json({ token: token });
+  } else {
+    res.json({ token: "invalid" });
+    // res.status(401).json({error: "Invalid Username or Password"});
+  }
 });
 
 app.listen(PORT, () => {
